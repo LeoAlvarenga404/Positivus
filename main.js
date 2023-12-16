d9f12b8a8836f4dabbcffd36cd9c4ea7cc7ae86d
leonardo <leonardo.palvarenga@gmail.com>
2023-12-16 11:09:18 -0300

    scroll.js add

---
+++ main.js
@@ -0,0 +1,24 @@
+
+
+const menu = document.querySelector('.menu')
+const nav =  document.querySelector('header nav')
+
+const activeElements = document.querySelectorAll('.collapse-container');
+
+activeElements.forEach(active => {
+  active.addEventListener('click', () => {
+    const collapseClick = active.closest('.collapse-container').querySelector('.collapse-click');
+    const collapseContainer = active.closest('.collapse-container');
+    
+    collapseClick.classList.toggle('hidden');
+    collapseContainer.classList.toggle('green');
+  });
+});
+
+
+menu.addEventListener('click', () => {
+  nav.classList.toggle('hidden')
+  menu.classList.toggle('rotate')
+  nav.classList.toggle('appear')
+})
+
