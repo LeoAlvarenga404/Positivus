0cbb96fd6d89706b0f421d7772931bd901338a29
leonardo <leonardo.palvarenga@gmail.com>
2023-12-17 13:53:29 -0300

    finish

--- scroll.js
+++ scroll.js
@@ -1,3 +1,21 @@
+document.addEventListener('DOMContentLoaded', function () {
+  const slider = document.querySelector('.slider');
+  const prevButton = document.getElementById('prev-button');
+  const nextButton = document.getElementById('next-button');
+
+  prevButton.addEventListener('click', function () {
+    scrollSlider(-1.1);
+  });
+  nextButton.addEventListener('click', function () {
+    scrollSlider(1.075);
+  });
+  function scrollSlider(direction) {
+    const scrollAmount = slider.clientWidth;
+    slider.scrollBy({ left: direction * scrollAmount, behavior: 'smooth' });
+  }
+});
+
+
 
 const myObserver = new IntersectionObserver((entries) => {
   entries.forEach((entry) => {
